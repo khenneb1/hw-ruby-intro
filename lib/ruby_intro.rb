b3a8e90d11c63d3a8779be9646a4652d352c6cf5
Ubuntu <ubuntu@ip-172-31-94-224.ec2.internal>
2020-01-31 21:27:30 +0000

    a few syntax changes

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,43 +2,40 @@
 
 # Part 1
 
-# Define a method sum(array) that takes an array of integers as an argument and returns the sum of its elements
 def sum arr
-  # YOUR CODE HERE
+# YOUR CODE HERE
   sum = 0
   arr.each { |i| sum += i}
   sum
 end
 
-# Define a method max_2_sum(array) which takes an array of integers as an argument and returns the sum of its two largest elements
 def max_2_sum arr
+# YOUR CODE HERE
   sum(arr.max(2))
 end
 
-# Define a method sum_to_n?(array, n) that takes an array of integers and an additional integer, n, as arguments and returns true if any two elements in the array of integers sum to n
 def sum_to_n? arr, n
+# YOUR CODE HERE
   arr.each do |i|
     arr.each do |j|
       return true if i != j && (i + j) == n
     end
   end 
-  
-  return false
+  false
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
-  return "Hello, " + name
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
-  if s.empty? || s[0].match(/\d/) || s[0].match(/\W/)
-    return false
-  end
+  return false if s.empty? || s[0].match(/\d/) || s[0].match(/\W/)
   return true unless s.start_with?('a','e','i','o', 'u','A','E','I','O','U')
+  false
 end
 
 def binary_multiple_of_4? s
@@ -52,6 +49,7 @@ end
 class BookInStock
 # YOUR CODE HERE
   attr_accessor :isbn, :price
+  
   def initialize(isbn, price)
     begin
       raise ArgumentError,
