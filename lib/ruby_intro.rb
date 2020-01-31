6ddb92cd8624621f76ff24821b20e0f818e977e2
Ubuntu <ubuntu@ip-172-31-94-224.ec2.internal>
2020-01-31 18:23:30 +0000

    Finished Part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,16 +2,28 @@
 
 # Part 1
 
+# Define a method sum(array) that takes an array of integers as an argument and returns the sum of its elements
 def sum arr
   # YOUR CODE HERE
+  sum = 0
+  arr.each { |i| sum += i}
+  sum
 end
 
+# Define a method max_2_sum(array) which takes an array of integers as an argument and returns the sum of its two largest elements
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum(arr.max(2))
 end
 
+# Define a method sum_to_n?(array, n) that takes an array of integers and an additional integer, n, as arguments and returns true if any two elements in the array of integers sum to n
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.each do |i|
+    arr.each do |j|
+      return true if i != j && (i + j) == n
+    end
+  end 
+  
+  return false
 end
 
 # Part 2
